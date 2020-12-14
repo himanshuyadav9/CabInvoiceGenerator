84d941bb7f7193239362761319ba1f76d352fe25
Himanshu <yadavhimanshu63@gmail.com>
2020-12-15 01:17:39 +0530

    for multiple rides

--- com/invoice/InvoiceService.java
+++ com/invoice/InvoiceService.java
@@ -4,13 +4,26 @@ public class InvoiceService {
 
 	private static final double MINIMUM_COST_PER_KM = 10;
 	private static final int COST_PER_TIME = 1;
+	private static final int MINIMUM_FARE = 5;
 
 	public double calculateFare(double distance, int time) {
-		return distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
+		double totalfare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
+		if(totalfare < MINIMUM_FARE)
+		   return MINIMUM_FARE;
+		return totalfare;
+	}
+	public double calculateFare(Ride[] rides) {
+		double totalfare = 0;
+		for (Ride ride : rides) {
+		totalfare += this.calculateFare(ride.distance, ride.time);
+		}
+		return totalfare;
 	}
 
 	public static void main(String[] args) {
 		InvoiceService invoice = new InvoiceService();
-		System.out.println("Cost " + invoice.calculateFare(2, 30));
+		System.out.println("Cost " + invoice.calculateFare(0.1, 1));
+		Ride [] rides = {new Ride(2.0, 5), new Ride(0.1 , 1)};
+		System.out.println("Cost " + invoice.calculateFare(rides));
 	}
 }
--- com/invoice/InvoiceServiceTest.java
+++ com/invoice/InvoiceServiceTest.java
@@ -15,5 +15,21 @@ public class InvoiceServiceTest {
 		double fare = invoice.calculateFare(distance , time);
 		Assert.assertEquals(25, fare , 0);
 	}
+	@Test
+	public void givenLessDistanceAndTime_ShouldReturnMinimumFare() {
+		InvoiceService invoice = new InvoiceService();
+		double distance = 0.1;
+		int time = 1;
+		double fare = invoice.calculateFare(distance , time);
+		Assert.assertEquals(5, fare , 0);
+	}
+	@Test
+	public void givenMultipleRides_ShouldReturnTotalFare() {
+		InvoiceService invoice = new InvoiceService();
+		Ride[] rides = {new Ride(2.0 , 5),
+				        new Ride(0.1 , 1)};
+		double fare = invoice.calculateFare(rides);
+		Assert.assertEquals(30, fare , 0);
+	}
 
 }
